11ba301751c590f9eb49359d24272b87a8d570df
damablanca <damablanca@users.noreply.github.com>
2017-04-12 23:16:17 +0300

    Chapter 6 ex.2: user entity, crud repository, UserDetailService, adding users, hashing passwords(BCrypt)

--- src/main/java/fi/haagahelia/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/BookstoreApplication.java
@@ -11,6 +11,8 @@ import fi.haagahelia.domain.Book;
 import fi.haagahelia.domain.BookRepository;
 import fi.haagahelia.domain.Category;
 import fi.haagahelia.domain.CategoryRepository;
+import fi.haagahelia.domain.User;
+import fi.haagahelia.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -22,24 +24,33 @@ public class BookstoreApplication {
 	
 	
 	@Bean
-	public CommandLineRunner demo(BookRepository repository, CategoryRepository crepository) {
+	public CommandLineRunner demo(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			log.info("Save some test data");
 			
+			//creating categories
 			crepository.save(new Category("Historical novel"));
 			crepository.save(new Category("Philosofical fiction"));
 			crepository.save(new Category("Horror novel"));
 			
+			//creating book information
 			repository.save(new Book("Sinuhe the Egyptian", "Mika Waltari",1945,"WM3445",20.60, crepository.findByName("Historical novel").get(0)));
 			repository.save(new Book("The unbearable lightness of being", "Milan Kundera",1985,"KM3573",10, crepository.findByName("Philosofical fiction").get(0)));
 			repository.save(new Book("The Picture of Dorian Gray", "Oscar Wilde",1890, "WO2899",15, crepository.findByName("Horror novel").get(0)));
 			repository.save(new Book("Perfume", "Patrick Suskind", 1985, "SP4879", 14.50, crepository.findByName("Horror novel").get(0)));
+			
+			//creating users User/user, Admin/admin
+			User user1 = new User("User","$2a$06$UNA9n39XY/Zy6hk/bAOn1OJTABNnyMQJZvLJQV6LYeMnGu1xIom.C","USER"); 
+			User user2 = new User("Admin","$2a$06$cxz4hplHx8HQ8Zw0eMw2Bu.rXZR44nUHtbl0xcGwpu2Q1c5OYIT.G","ADMIN"); 
+			urepository.save(user1);
+			urepository.save(user2);
 			
 			log.info("Fetch book information");
 			for (Book book : repository.findAll()) {
 				log.info(book.toString());
 			} 
 		};
+		
 	} 
 
 }
--- src/main/java/fi/haagahelia/WebSecurityConfig.java
+++ src/main/java/fi/haagahelia/WebSecurityConfig.java
@@ -3,13 +3,21 @@ package fi.haagahelia;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import fi.haagahelia.web.UserDetailServiceImpl;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserDetailServiceImpl userDetailsService;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -29,10 +37,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.inMemoryAuthentication()
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+		
+		
+			/*.inMemoryAuthentication()
 				.withUser("User").password("Password").roles("USER")
 				.and()
-				.withUser("Admin").password("Password").roles("USER", "ADMIN");
+				.withUser("Admin").password("Password").roles("USER", "ADMIN");*/
 		}
 	}
